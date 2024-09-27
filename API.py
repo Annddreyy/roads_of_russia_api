09c0f872938825926291ea71c3ea6490fea73291
Annddreyy <andreymail22112006@gmail.com>
2024-09-27 22:05:04 +0300

    Add resumes

--- API.py
+++ API.py
@@ -36,7 +36,7 @@ def get_news():
                 'id': one_news[0],
                 'title': one_news[1],
                 'description': one_news[2],
-                'date': one_news[3],
+                'date': str(one_news[3]),
                 'author': f'{author[0]}.{author[1][0]}.{author[2][0]}'
             }
         )
@@ -45,6 +45,34 @@ def get_news():
     conn.close()
 
     return jsonify(news_json)
+
+@app.route('/resume', methods=['GET'])
+def get_resume():
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute('SELECT * FROM resume')
+
+    resumes = cur.fetchall()
+
+    resumes_json = []
+    for resume in resumes:
+        job_id = resume[5]
+        cur.execute(f'SELECT title FROM job_title WHERE job_title_id={job_id}')
+        job = cur.fetchone()
+        resumes_json.append(
+            {
+                'id': resume[0],
+                'author': f'{resume[1]} {resume[2][0]}. {resume[3][0]}.',
+                'file_path': resume[4].split('/')[-1],
+                'job_title': job[0]
+            }
+        )
+
+    cur.close()
+    conn.close()
+
+    return jsonify(resumes_json)
 
 if __name__ == '__main__':
     app.run(port=2345)
